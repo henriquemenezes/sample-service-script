b6757ebb2402a9ecd15ca61cd9644eebff4b0ecf
Henrique Menezes <floresta.henrique@gmail.com>
2015-01-12 12:06:16 -0300

    Update new-service.sh

--- new-service.sh
+++ new-service.sh
@@ -3,7 +3,7 @@
 SERVICE_FILE=$(tempfile)
 
 echo "--- Download template ---"
-wget -q -O "$SERVICE_FILE" 'https://raw.githubusercontent.com/jasonblewis/sample-service-script/master/service.sh' || { echo 'ERROR: Could not retreive service.sh from github'; exit 1;}
+wget -q -O "$SERVICE_FILE" 'https://raw.githubusercontent.com/henriquemenezes/sample-service-script/master/service.sh' || { echo 'ERROR: Could not retreive service.sh from github'; exit 1;}
 
 chmod +x "$SERVICE_FILE"
 echo ""
